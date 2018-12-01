d3bb90d6b44a248c4414b2ff2fefba61ca75de5a
Praveen Pal <praveenpal4232@gmail.com>
2018-12-01 19:22:02 +0530

    Simple website based on Tech Event
    Event-Bee

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+  document.getElementById('header').append(ball);
+}
+
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000,
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+$('#sU').click(function() {
+  // $('#regs').animate({left: '0'});
+  // $('#regs').css('visibility', 'visible');
+  // $('#regs').css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, 200);
+  $("#regs").css({visibility:"visible", opacity: 0.0, display: "block"}).animate({opacity: 1.0}, 1);
+});
+
+$('#lg').click(function() {
+  // $('#regs').animate({left: '0'});
+  // $('#regs').css('visibility', 'visible');
+  $('#regs').css({opacity: 1.0, visibility: "hidden"}).animate({opacity: 0}, 200);
+  // $('#regs').css({display: 'none'});
+  // $("#regs").css({visibility:"visible", opacity: 0.0, display: "block"}).animate({opacity: 1.0}, 1);
+});
