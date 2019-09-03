8863458b12d99c13f48eac6e544d0910b2aeb832
Praveen Pal <praveenpal4232@gmail.com>
2019-09-03 22:18:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36","#FFD700"];
 
 const numBalls = 100;
 const balls = [];
